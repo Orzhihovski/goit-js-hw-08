f2d42d1d965c8772ced09b02117ea190d2d8b7a7
Dimass16 <dmitkach1@gmail.com>
2023-04-27 16:58:50 +0300

    commit3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,25 +1,34 @@
-import SimpleLightbox from 'simplelightbox'; 
-import 'simplelightbox/dist/simple-lightbox.min.css'; 
 import { galleryItems } from './gallery-items'; 
+import SimpleLightbox from "simplelightbox"; 
+import "simplelightbox/dist/simple-lightbox.min.css"; 
+// Change code below this line 
+const galleryEl = document.querySelector('.gallery'); 
  
-console.log(galleryItems); 
-const galleryContainer = document.querySelector('.gallery'); 
-function createMarkup(items) { 
-  return items 
-    .map(({ preview, original, description }) => { 
-      return ` 
-        <li> 
-          <a class="gallery__item" href="${original}"> 
-            <img style="display: block" class="gallery__image" src="${preview}" alt="${description}"/> 
-          </a> 
-        </li> 
-  `; 
-    }) 
-    .join(''); 
+function makeGalleryImgMarkup({ preview, original, description }) { 
+ return ` 
+  <a class="gallery__item" href="${original}"> 
+    <img 
+      class="gallery__image" 
+      src="${preview}" 
+      alt="${description}" 
+    /> 
+  </a>`; 
 } 
-const galleryMarkup = createMarkup(galleryItems); 
-galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup); 
-let gallery = new SimpleLightbox('.gallery a', { 
-  captionsData: 'alt', 
-  captionDelay: 250, 
-});
+ 
+function makeGalleryMarkup(array) { 
+ return array.map(makeGalleryImgMarkup).join(''); 
+} 
+function renderMarkup(markup) { 
+ galleryEl.insertAdjacentHTML('afterbegin', markup); 
+} 
+const galleryMarkup = makeGalleryMarkup(galleryItems); 
+renderMarkup(galleryMarkup); 
+ 
+const lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: "alt", 
+    captionDelay: 250, 
+    overlayOpacity: 0.9, 
+ 
+}); 
+ 
+// console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
-
+import throttle from 'lodash.throttle'; 
+ 
+const form = document.querySelector('.feedback-form'); 
+const email = document.querySelector('.form-email'); 
+const textarea = document.querySelector('.form-textarea'); 
+const button = document.querySelector('.form-button'); 
+ 
+if (localStorage.getItem('feedback-form-state') !== null) { 
+  const saveData = JSON.parse(localStorage.getItem('feedback-form-state')); 
+  email.value = saveData.email; 
+  textarea.value = saveData.message; 
+} else { 
+  email.value = ''; 
+  textarea.value = ''; 
+} 
+ 
+form.addEventListener('input', throttle(onFormInputEvents, 500)); 
+ 
+function onFormInputEvents(event) { 
+  localStorage.setItem( 
+    'feedback-form-state', 
+    JSON.stringify({ 
+      email: email.value, 
+      message: textarea.value, 
+    }) 
+  ); 
+} 
+ 
+form.addEventListener('submit', onClickButtonSubmit); 
+ 
+function onClickButtonSubmit(event) { 
+  event.preventDefault(); 
+ 
+  email.value = ''; 
+  textarea.value = ''; 
+ 
+  console.log( 
+    localStorage.getItem( 
+      'feedback-form-state', 
+      JSON.stringify({ 
+        email: email.value, 
+        message: textarea.value, 
+      }) 
+    ) 
+  ); 
+ 
+  localStorage.clear(); 
+}
